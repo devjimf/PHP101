be6e4c9910e17815ce6f297592712be0b9ce809c
Fdc.DevJimf <118711286+devjimf@users.noreply.github.com>
2023-04-12 14:33:28 +0800

    Problem number 2: easy

--- Basics/problem1.php
+++ Basics/problem1.php
@@ -1,5 +1,7 @@
 <?php 
 
+// problem # 1
+// Complexity: easy
 // Write a PHP program that takes an array of integers as input and 
 // returns the sum of all the even numbers in the array.
 
@@ -9,7 +11,6 @@
 
 // Output: 12
 
-    
     function addEven($arr) {
         $total = 0;
         foreach($arr as $num){
@@ -27,6 +28,36 @@
 
     echo $result;
 
+// Problem # 2
+// Complexity: easy
+// Write a PHP program that takes a string as input and checks if it is a palindrome.
+
+// A palindrome is a word or phrase that reads the same forwards and backwards. 
+// Here are some examples of palindromes:
+
+// racecar
+// level
+// A man, a plan, a canal, Panama!
+
+    function isPalindrome($word){
+
+        $word = str_replace("/[^A-Za-z0-9]/",'', $word );
+        
+        if($word == strrev($word)){
+
+            return 'Palindrome';
+
+        } else {
+
+            return 'Not Palindrome';
+
+        }
+
+    }
     
+    echo '<pre>';
+    echo isPalindrome('racecar');
+    echo isPalindrome('level');
+    echo isPalindrome('A man, a plan, a canal, Panama!');
 
 ?>
